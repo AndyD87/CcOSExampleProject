b7df580a5a3f1c8ccb540e36fb671561fb23aa65
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-09-24 21:00:36 +0200

    Ressource added, CcOS updated

---
+++ CcOSExampleProject/Resources/CcOSExampleProject.json.c
@@ -0,0 +1,6 @@
+// Resource file generated from CcOSResource
+#include "CcBase.h"
+const char* CcOSExampleProject_Json = "\
+{}\n\
+";
+size_t CcOSExampleProject_Json_Length = 3; 
---
+++ CcOSExampleProject/Resources/CcOSExampleProject.json.h
@@ -0,0 +1,4 @@
+// Resource file generated from CcOSResource
+#include "CcBase.h"
+CCEXTERNC const char* CcOSExampleProject_Json;
+CCEXTERNC size_t CcOSExampleProject_Json_Length; 
